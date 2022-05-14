7bf6394e14b774fe0a3f7216dbfbdbe1901d3929
Алексей Чернышев <chernyshev.prog@gmail.com>
2022-05-14 09:30:11 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,41 @@
+# Импорт недавно установленного пакета setuptools.
+import setuptools
+
+# Открытие README.md и присвоение его long_description.
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# Определение requests как requirements для того, чтобы этот пакет работал. Зависимости проекта.
+# requirements = ["requests<=2.21.0"]
+
+# Функция, которая принимает несколько аргументов. Она присваивает эти значения пакету.
+setuptools.setup(
+    # Имя дистрибутива пакета. Оно должно быть уникальным, поэтому добавление вашего имени пользователя в конце является обычным делом.
+    name="dir_to_crx",
+    # Номер версии вашего пакета. Обычно используется семантическое управление версиями.
+    version="1.0.0",
+    # Имя автора.
+    author="Aleksey Chernyshev",
+    # Его почта.
+    author_email="chernyshev.prog@gmail.com",
+    # Краткое описание, которое будет показано на странице PyPi.
+    description="Convert directory to .crx for google extension",
+    # Длинное описание, которое будет отображаться на странице PyPi. Использует README.md репозитория для заполнения.
+    long_description=long_description,
+    # Определяет тип контента, используемый в long_description.
+    long_description_content_type="text/markdown",
+    # URL-адрес, представляющий домашнюю страницу проекта. Большинство проектов ссылаются на репозиторий.
+    url="https://github.com/ericjaychi/sample-pypi-package",
+    # Находит все пакеты внутри проекта и объединяет их в дистрибутив.
+    packages=setuptools.find_packages(),
+    # requirements или dependencies, которые будут установлены вместе с пакетом, когда пользователь установит его через pip.
+    # install_requires=requirements,
+    # Предоставляет pip некоторые метаданные о пакете. Также отображается на странице PyPi.
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # Требуемая версия Python.
+    python_requires='>=3.6',
+)
---
+++ src/create.py
@@ -0,0 +1,119 @@
+import os
+import zipfile
+from crx3_pb2 import SignedData, CrxFileHeader
+import struct
+import io
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, utils, padding
+
+fileBufferLength = 4096
+
+
+def create_publickey(private_key):
+    public_key = private_key.public_key()
+    data = public_key.public_bytes(
+        encoding=serialization.Encoding.DER,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo,
+    )
+    return data
+
+
+def get_zipped_data_and_basename_from_dir(dir):
+    if os.path.isdir(dir):
+        return zipdir(dir), dir
+    else:
+        raise IOError('Source is not a directory or zip file <%s>' % dir)
+
+
+def package(_dir, output):
+    zipdata, basename = get_zipped_data_and_basename_from_dir(_dir)
+
+    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
+    public_key = create_publickey(private_key)
+
+    signed_header_data_str = create_signed_header_data_str(public_key)
+
+    signed = sign(signed_header_data_str, zipdata, private_key)
+    header_str = create_header_str(public_key, signed, signed_header_data_str)
+
+    save_crx_file(header_str, zipdata, output, basename)
+
+
+def sign(signed_header_data_str, zipped, private_key):
+    signed_header_size_octets = struct.pack('<I', len(signed_header_data_str))
+
+    chosen_hash = hashes.SHA256()
+    hasher = hashes.Hash(chosen_hash, default_backend())
+    hasher.update(b'CRX3 SignedData\00')
+    hasher.update(signed_header_size_octets)
+    hasher.update(signed_header_data_str)
+
+    for i in range(0, len(zipped), fileBufferLength):
+        if i + fileBufferLength <= len(zipped):
+            hasher.update(zipped[i: i + fileBufferLength])
+        else:
+            hasher.update(zipped[i: len(zipped)])
+
+    digest = hasher.finalize()
+
+    return private_key.sign(digest, padding.PKCS1v15(), utils.Prehashed(chosen_hash))
+
+
+def zipdir(directory, inject=None):
+    zip_memory = io.BytesIO()
+    with zipfile.ZipFile(zip_memory, 'w', zipfile.ZIP_DEFLATED) as zf:
+
+        def _rec_zip(path, parent='', inject=None):
+            if inject:
+                for fname, fdata in inject.items():
+                    zf.writestr(fname, fdata)
+
+            for d in os.listdir(path):
+                child = os.path.join(path, d)
+                name = '%s/%s' % (parent, d)
+                if os.path.isfile(child):
+                    zf.write(child, name)
+                if os.path.isdir(child):
+                    _rec_zip(child, name)
+
+        _rec_zip(directory, '', inject=inject)
+        zf.close()
+        zipdata = zip_memory.getvalue()
+        return zipdata
+
+
+def get_crx_id(public_key):
+    digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+    digest.update(public_key)
+    hased = digest.finalize()
+    return hased[0:16]
+
+
+def create_signed_header_data_str(public_key):
+    signed_header_data = SignedData()
+    signed_header_data.crx_id = get_crx_id(public_key)
+    return signed_header_data.SerializeToString()
+
+
+def create_header_str(public_key, signed, signed_header_data_str):
+    header = CrxFileHeader()
+    proof = header.sha256_with_rsa.add()
+    proof.public_key = public_key
+    proof.signature = signed
+    header.signed_header_data = signed_header_data_str
+    return header.SerializeToString()
+
+
+def save_crx_file(header_str, zipped, path, crdx):
+    header_size_octets = struct.pack('<I', len(header_str))
+
+    fileLocation = path if path else '%s.crx' % crdx
+    with open(fileLocation, 'wb') as crx:
+        data = [b'Cr24', struct.pack('<I', 3), header_size_octets, header_str, zipped]
+        for d in data:
+            crx.write(d)
+
+
+def create(path_extension: str, path_save: str = ''):
+    return package(path_extension, path_save)
